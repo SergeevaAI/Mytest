4961ce1ed8b59cad2b8c923367ed59eb799df398
SergeevaAI <90114269+SergeevaAI@users.noreply.github.com>
2022-11-23 18:59:18 +0300

    Add files via upload

---
+++ hello_operator.py
@@ -0,0 +1,6 @@
+from typing import Union, Iterable, Dict
+from airflow.models import BaseOperator, SkipMixin
+
+class HelloOperator(BaseOperator, SkipMixin):
+  def execute(self, context):
+    self.logger.info("Hello, World!") 
